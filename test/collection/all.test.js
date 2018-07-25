f0c49ed245ec6b0a7e97881325c39561591a7961
axelvaindal <pro.axelvaindal@gmail.com>
2018-07-25 11:43:42 +0200

    Fix bug on any methods

--- test/collection/all.test.js
+++ test/collection/all.test.js
@@ -5,8 +5,13 @@ const isNumber = require("../../lib/object/isNumber.js");
 
 describe("all", function () 
 {
-    it("Should apply function to a collection", function () 
+    it("Should return true when all element passes a predicate.", function () 
     {
         all([0, 1, 2], isNumber).should.be.exactly(true);
+    });
+
+    it("Should return false when one element doesn't pass a predicate.", function () 
+    {
+        all([0, 1, "titi"], isNumber).should.be.exactly(false);
     });
 });
--- test/collection/any.test.js
+++ test/collection/any.test.js
@@ -1,12 +1,19 @@
 require("should");
 
-const any = require("../../lib/collection/all.js");
+const any = require("../../lib/collection/any.js");
 const isNumber = require("../../lib/object/isNumber.js");
 
 describe("any", function () 
 {
-    /*it("Should apply function to a collection", function () 
+    it("Should return true whenever an element passes a predicate.", function () 
     {
-        any([0, 1, true], isNumber).should.be.exactly(true);
-    });*/
+        let result = any(["titi", "toto", 0], isNumber);
+        result.should.be.exactly(true);
+    });
+
+    it("Should return false when no elements passes a predicate.", function () 
+    {
+        let result = any(["titi", "toto", true], isNumber);
+        result.should.be.exactly(false);
+    });
 });
