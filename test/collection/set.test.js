d7207abdece68cecd54d4fbe5ff09d1a9fe03ef3
Axel Vaindal <pro.axelvaindal@gmail.com>
2019-07-20 18:10:49 +0200

    feat: Set now creates unexisting path
    set functions now creates unexisting path if necessary.
    Numeric path are created as arrays, litteral ones are created as object.

--- test/collection/set.test.js
+++ test/collection/set.test.js
@@ -1,22 +1,34 @@
 require("should");
 
-const set = require("../../lib/collection/set.js");
+const set = require("../../lib/collection/set");
 
-describe("set", function()
-{
-    it("Should add a value to a collection.", function()
-    {
-        let col = [];
-        col = set(col, 0, "test");
+describe("set", () => {
+  it("Should add a value to a collection.", () => {
+    let col = [];
+    col = set(col, 0, "test");
 
-        col.should.eql(["test"]);
-    });
+    col.should.eql(["test"]);
+  });
 
-    it("Should set a value to a collection.", function()
-    {
-        let col = [1];
-        col = set(col, 0, "test");
+  it("Should set a value to a collection.", () => {
+    let col = [1];
+    col = set(col, 0, "test");
 
-        col.should.eql(["test"]);
-    });
+    col.should.eql(["test"]);
+  });
+
+  it("Should create missing property as array", () => {
+    let col = [];
+    col = set(col, "1.0", "test");
+
+    col.length.should.eql(2);
+    col.indexOf(undefined).should.eql(-1);
+    col[1].should.eql(["test"]);
+  });
+
+  it("Should create missing property as object", () => {
+    let col = {};
+    col = set(col, "a.b", 3);
+    col.should.eql({a: {b: 3}});
+  });
 });
